89b03b3fe6b5cc55c5f2507ff0939edc33825a7d
Gaurav <gk5480325@gmail.com>
2025-07-14 14:29:24 +0530

    cookie header is done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import uvicorn
-from fastapi import FastAPI, Path, Body, Request, Form
+from fastapi import FastAPI, Path, Body, Request, Form, Cookie
 from typing import List
 from pydantic import BaseModel, Field
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -19,6 +19,21 @@ class Student(BaseModel):
 class User(BaseModel):
     username: str
     password: str
+
+
+
+@app.post("/cookie/")
+def create_cookie():
+    content = {"message" : "cookie_set"}
+    response = JSONResponse(content=content)
+    response.set_cookie(key="usrname", value="admin")
+    return response
+
+@app.get("/readcookie/")
+async def read_cookie(usrname: str = Cookie(None)):
+    return { "username": usrname }
+
+
 
 
 @app.get("/login/", response_class=HTMLResponse)
